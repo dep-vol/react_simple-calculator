e56487b21fa1cfa58f58b3f36c9dfef151d32dae
dep-vol <alex.tecto.msk@gmail.com>
2020-05-31 06:44:25 +0300

    Updates

---
+++ precache-manifest.05b3620a4e100000951fd795a4842e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be533981bbe1e95ee0384ba673d03d8d",
+    "url": "/react_simple-calculator/index.html"
+  },
+  {
+    "revision": "cd5138e8ce464893e252",
+    "url": "/react_simple-calculator/static/css/main.bc70b41b.chunk.css"
+  },
+  {
+    "revision": "5b1c440082149d708498",
+    "url": "/react_simple-calculator/static/js/2.a67528c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_simple-calculator/static/js/2.a67528c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5138e8ce464893e252",
+    "url": "/react_simple-calculator/static/js/main.4522d18e.chunk.js"
+  },
+  {
+    "revision": "492567e8d98a078cfde6",
+    "url": "/react_simple-calculator/static/js/runtime-main.add42616.js"
+  }
+]);
